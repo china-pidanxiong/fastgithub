d6519770cfe361101f7430d13355f6ab2ea3d1a2
跃超的拌面 <zhang.liuliu66@petalmail.com>
2026-06-29 21:51:32 +0800

    feat(ui): 主窗口重构为侧边栏导航布局
    - 顶部标题栏：Octocat 图标 + 标题 + 版本号 + 状态 + 刷新按钮
    - 左侧 160px 侧边栏：仪表盘/配置/日志三个导航项
    - 左侧选中指示条 + 蓝色高亮效果
    - MainWindowViewModel 继承 ViewModelBase，支持页面切换

--- src/FastGithub/MainWindow.xaml.cs
+++ src/FastGithub/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using FastGithub.Services;
+using FastGithub.ViewModels;
 
 namespace FastGithub;
 
@@ -13,6 +16,24 @@ public partial class MainWindow : Window
     public MainWindow() => InitializeComponent();
 
     public void SetTrayService(TrayService trayService) => _trayService = trayService;
+
+    private void Dashboard_Click(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is MainWindowViewModel vm)
+            ContentArea.Content = new Views.DashboardView { DataContext = vm.Dashboard };
+    }
+
+    private void Config_Click(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is MainWindowViewModel vm)
+            ContentArea.Content = new Views.ConfigView { DataContext = vm.Config };
+    }
+
+    private void Log_Click(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is MainWindowViewModel vm)
+            ContentArea.Content = new Views.LogView { DataContext = vm.Log };
+    }
 
     private void Window_Closing(object sender, CancelEventArgs e)
     {
--- src/FastGithub/ViewModels/MainWindowViewModel.cs
+++ src/FastGithub/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,27 @@
+using System.Windows.Input;
+
 namespace FastGithub.ViewModels;
 
-public class MainWindowViewModel
+public class MainWindowViewModel : ViewModelBase
 {
     public DashboardViewModel Dashboard { get; }
     public ConfigViewModel Config { get; }
     public LogViewModel Log { get; }
+
+    private int _selectedIndex;
+    public int SelectedIndex { get => _selectedIndex; set => SetProperty(ref _selectedIndex, value); }
+
+    public ICommand SelectDashboardCommand { get; }
+    public ICommand SelectConfigCommand { get; }
+    public ICommand SelectLogCommand { get; }
 
     public MainWindowViewModel(DashboardViewModel dashboard, ConfigViewModel config, LogViewModel log)
     {
         Dashboard = dashboard;
         Config = config;
         Log = log;
+        SelectDashboardCommand = new RelayCommand(_ => SelectedIndex = 0);
+        SelectConfigCommand = new RelayCommand(_ => SelectedIndex = 1);
+        SelectLogCommand = new RelayCommand(_ => SelectedIndex = 2);
     }
 }
